d0d9cd22f6971ccc40dfa29b135df46ce368d31d
sbigelo <sean.bigelow1@gmail.com>
2021-03-11 12:19:46 -0500

    moved around associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :comments
-    has_many :commented_reviews, through: :comments
+    has_many :commented_reviews, through: :comments, source: :review
 
     has_many :reviews
     # has_many :cars, through: :reviews
