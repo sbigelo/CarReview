bebbb91a0e1b5e5d67591cdb0fbcc14d15e5ec01
sbigelo <sean.bigelow1@gmail.com>
2021-03-11 16:24:54 -0500

    added a flash alert for wrong login info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
 
-    def home
-
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:message] = "Wrong login info, please try again."
+            redirect_to '/login'
+        end
     end
 
     def destroy
-        session.destroy
+        session.clear
         redirect_to root_path
     end
 
 
 end
+
+
+
+   
