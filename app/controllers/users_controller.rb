10cc68f1ed3b29178bc1afda9f0d6ac4e521bba9
sbigelo <sean.bigelow1@gmail.com>
2021-03-17 14:52:36 -0400

    added a helper method for showing reveiws/proper on a users page, re-arranged users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     def show
         redirect_if_not_logged_in
-        @user = User.find_by(id: params[:id])
+        @user = User.find_by_id(params[:id])
         redirect_to root_path if !@user
     end
 
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,15 @@
 module ReviewsHelper
+
+    def show_reviews
+        if @user.reviews.empty?
+            content_tag(:h1, "No Reviews have been made yet.")
+        elsif @user == current_user
+            content_tag(:h1, "Welcome, #{@user.username}! Here are your reviews: ")
+        else
+            content_tag(:h1, "Here are their reviews:")
+        end
+    end
+
 end
+
+
