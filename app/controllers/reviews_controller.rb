637d8998b81e1b2dcf195f083aab82cd717e9ca8
sbigelo <sean.bigelow1@gmail.com>
2021-03-18 13:05:30 -0400

    added update and edit for reviews on controller and views, added a render partial to not dry

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,21 @@ class ReviewsController < ApplicationController
         end
     end
 
+    def edit
+        @review = Review.find(params[:id])
+        redirect_to reviews_path if !@review || @review.user != current_user
+    end
+
+    def update
+        @review = Review.find_by_id(params[:id])
+        redirect_to reviews_path if !@review || @review.user != current_user
+        if @review.update(review_params)
+            redirect_to review_path(@review)
+        else
+            render :edit
+        end
+    end
+
     def index
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
             @reviews = @user.reviews
