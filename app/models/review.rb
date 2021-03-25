447f1ba45113dd1ae0130fef252aff4cfa48bdda
sbigelo <sean.bigelow1@gmail.com>
2021-03-25 14:26:07 -0400

    added a render partial for new comments errors, changed something that somehow messed up oauth login

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,10 @@ class Review < ApplicationRecord
   # belongs_to :car
   has_many :comments
   has_many :users, through: :comments
+
   validates :content, presence: true
   validates  :title, presence: true
+  validates :content, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 
   scope :abc, -> {order(:title)}
   scope :most_comments, -> {left_outer_joins(:comments).group('reviews.id').order('count(reviews.id) desc')}
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     has_many :comments
     has_many :commented_reviews, through: :comments, source: :review
-    validates :password, length: { in: 6..20 }
+    
     has_many :reviews
     # has_many :cars, through: :reviews
     validates :username, :email, uniqueness: true, presence: true
-    validates :username, length: { in: 6..20 }
+    
 
 end
