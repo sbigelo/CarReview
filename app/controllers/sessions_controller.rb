58a0cd345484da9e56389306961917df9732bd00
sbigelo <sean.bigelow1@gmail.com>
2021-03-25 14:05:37 -0400

    added validations for minimum password and username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            redirect_to '/'
+            redirect_to root_path
         end
     end
 
