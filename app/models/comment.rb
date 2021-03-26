4e466a5e0342ec577467724bdc6f7c7ef0d50d0d
sbigelo <sean.bigelow1@gmail.com>
2021-03-26 17:35:14 -0400

    added error render partial for comments/edit and reviews/edit, also added validation of one comment per review for each user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ApplicationRecord
 
     validates :content, presence: true
     validates :content, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
-    
+    validates :review, uniqueness: {scope: :user_id, message: "comment can only have one comment per person."}
 end
 
 
